c9e92998d0bad4c77e333250a78683beea9d7eb8
ngochuy9123 <ngochuy09123@gmail.com>
2024-01-13 22:30:10 +0700

    exception handle

--- src/main/java/com/example/blog_api/BlogApiApplication.java
+++ src/main/java/com/example/blog_api/BlogApiApplication.java
@@ -1,11 +1,18 @@
 package com.example.blog_api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.ui.Model;
 
 @SpringBootApplication
 public class BlogApiApplication {
 //Hello
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
     public static void main(String[] args) {
         SpringApplication.run(BlogApiApplication.class, args);
     }
--- src/main/java/com/example/blog_api/payload/PostDto.java
+++ src/main/java/com/example/blog_api/payload/PostDto.java
@@ -2,11 +2,13 @@ package com.example.blog_api.payload;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDto {
     private long id;
     private String title;
     private String description;
     private String content;
-
+    private Set<CommentDto> comments;
 }
